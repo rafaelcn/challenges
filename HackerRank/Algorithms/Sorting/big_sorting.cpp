eb2d762e15e9be6bd2e81be5ce88a71894efbfaa
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-08-23 03:06:48 -0300

    Add c++ solution to the Big Sorting problem
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ HackerRank/Algorithms/Sorting/big_sorting.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize ("Ofast")
+
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+
+    int n;
+    cin >> n;
+
+    vector<string> to_sort(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> to_sort[i];
+    }
+
+    // Sort using the lexographical order of the string.
+    // Converting from/to a unsigned long long number isn't feasible due
+    // to the problem constraint.
+    sort(to_sort.begin(), to_sort.end(), [](const string a, const string b) {
+            int sa = a.size();
+            int sb = b.size();
+
+            if (sa == sb) {
+                return sb < sa;
+            }
+
+            return b < a;
+    });
+
+    for (const auto& e: to_sort) {
+        cout << e << "\n";
+    }
+
+    return 0;
+}
