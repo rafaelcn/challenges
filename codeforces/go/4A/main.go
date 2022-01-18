0b32d7dcce4e8c2cd0a257524e064a4b363c8d3b
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-01-18 20:38:44 -0300

    Add a new solution for the 4A problem
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ codeforces/go/4A/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var w int
+
+	fmt.Scanf("%d", &w)
+
+	// e + e = e
+	// o + o = e
+	// e + o = o
+
+	if w&1 == 1 {
+		fmt.Print("NO")
+	} else {
+		if factors(w) {
+			fmt.Print("YES")
+		} else {
+			fmt.Print("NO")
+		}
+	}
+}
+
+func factors(n int) bool {
+
+	for i := 2; i < n; i += 2 {
+		for j := 2; j+i <= n; j += 2 {
+			if i+j == n {
+				return true
+			}
+		}
+	}
+
+	return false
+}
