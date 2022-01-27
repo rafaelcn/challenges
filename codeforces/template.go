a2d30841bbf833c1ed328232077c25bd4471c98d
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-01-27 17:01:15 -0300

    Update go template
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- codeforces/template.go
+++ codeforces/template.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func solve() {
-	
+
 }
 
 func main() {
@@ -17,3 +17,15 @@ func main() {
 		t--
 	}
 }
+
+// Data structures
+
+type Pair struct {
+	First, Second int
+}
+
+type Pairs []Pair
+
+func (p Pairs) Len() int           { return len(p) }
+func (p Pairs) Less(i, j int) bool { return p[i].First < p[j].First }
+func (p Pairs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
