746fc7209c5c7d6543996f2e44ffb5fe6db8cdcd
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-02-13 09:50:23 -0300

    Add solution for problem A
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ codeforces/contests/1421/A.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solve() {
+
+	var a, b int
+
+	scanf("%d %d\n", &a, &b)
+
+	println(a^b)
+}
+
+func main() {
+
+	var t int
+
+	scanf("%d\n", &t)
+
+	for t > 0 {
+		solve()
+		t--
+	}
+
+	writer.Flush()
+}
+
+// Use buffering when reading/writing data as the default fmt.Scanx/fmt.Printx
+// functions doesn't.
+
+var (
+	writer = bufio.NewWriter(os.Stdout)
+	reader = bufio.NewReader(os.Stdin)
+)
+
+func scanf(f string, a ...interface{}) {
+	fmt.Fscanf(reader, f, a...)
+}
+
+func printf(f string, a ...interface{}) {
+	fmt.Fprintf(writer, f, a...)
+}
+
+func println(a ...interface{}) {
+	fmt.Fprintln(writer, a...)
+}
+
+// Data structures
+
+type Pair struct {
+	First, Second int
+}
+
+type Pairs []Pair
+
+func (p Pairs) Len() int           { return len(p) }
+func (p Pairs) Less(i, j int) bool { return p[i].First < p[j].First }
+func (p Pairs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// Utils Functions
+
+func max(a, b int) int {
+
+	if a > b {
+		return a
+	}
+
+	return b
+}
