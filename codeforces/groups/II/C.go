7daf3f6ba8ef1063d4cd432624bf1fd0e66dd490
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-02-27 19:04:16 -0300

    Add solution for problem C
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ codeforces/groups/II/C.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/bits"
+	"os"
+)
+
+// C. ANDré
+
+func solve() {
+
+	var n uint
+	scanf("%d\n", &n)
+
+	b := bits.Len(n)
+
+	println((1 << (b - 1)) - 1)
+}
+
+func main() {
+
+	var t int
+
+	scanf("%d\n", &t)
+
+	for t > 0 {
+		solve()
+		t--
+	}
+
+	writer.Flush()
+}
+
+// Use buffering when reading/writing data as the default fmt.Scanx/fmt.Printx
+// functions doesn't.
+
+var (
+	writer = bufio.NewWriter(os.Stdout)
+	reader = bufio.NewReader(os.Stdin)
+)
+
+func scanf(f string, a ...interface{}) {
+	fmt.Fscanf(reader, f, a...)
+}
+
+func printf(f string, a ...interface{}) {
+	fmt.Fprintf(writer, f, a...)
+}
+
+func println(f ...interface{}) {
+	fmt.Fprintln(writer, f...)
+}
+
+// Data structures
+
+type Pair struct {
+	First, Second int
+}
+
+type Pairs []Pair
+
+func (p Pairs) Len() int           { return len(p) }
+func (p Pairs) Less(i, j int) bool { return p[i].First < p[j].First }
+func (p Pairs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
