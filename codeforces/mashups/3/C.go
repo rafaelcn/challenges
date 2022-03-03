dc4a79df9ec80e8b47968343ece1d3f2ce283a34
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-03-03 00:42:32 -0300

    Add incomplete solutions of past mashups
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ codeforces/mashups/3/C.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// C. Números na Mesa
+
+func solve() {
+
+	var n int
+	scanf("%d\n", &n)
+
+	a := []int{}
+	for i := 0; i < n; i++ {
+		scanf("%d", &a[i])
+	}
+	scanf("\n")
+
+	k := -1
+
+	for i := 0; i <= 1024; i++ {
+		// TODO
+	}
+
+	println(k)
+}
+
+func main() {
+
+	var t int
+
+	scanf("%d\n", &t)
+
+	for t > 0 {
+		solve()
+		t--
+	}
+
+	writer.Flush()
+}
+
+// Use buffering when reading/writing data as the default fmt.Scanx/fmt.Printx
+// functions doesn't.
+
+var (
+	writer = bufio.NewWriter(os.Stdout)
+	reader = bufio.NewReader(os.Stdin)
+)
+
+func scanf(f string, a ...interface{}) {
+	fmt.Fscanf(reader, f, a...)
+}
+
+func printf(f string, a ...interface{}) {
+	fmt.Fprintf(writer, f, a...)
+}
+
+func println(f ...interface{}) {
+	fmt.Fprintln(writer, f...)
+}
+
+// Data structures
+
+type Pair struct {
+	First, Second int
+}
+
+type Pairs []Pair
+
+func (p Pairs) Len() int           { return len(p) }
+func (p Pairs) Less(i, j int) bool { return p[i].First < p[j].First }
+func (p Pairs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
