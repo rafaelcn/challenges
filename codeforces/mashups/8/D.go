8aa62066f51ac8fdcc312a593d1daa89e5cea620
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-04-06 23:16:44 -0300

    Add solution for problem D
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ codeforces/mashups/8/D.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// D.
+
+type Tuple struct {
+	Number    int
+	Index     int
+	IndexShot int
+}
+
+type Tuples []Tuple
+
+func main() {
+
+	var n, ni, shots int
+	fmt.Scanf("%d\n", &n)
+
+	tuples := make(Tuples, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &ni)
+
+		tuples[i].Index = i
+		tuples[i].Number = ni
+	}
+	//fmt.Scanf("\n")
+
+	// sort first by the number
+	sort.Slice(tuples, func(i, j int) bool { return tuples[i].Number > tuples[j].Number })
+
+	for i, v := range tuples {
+		shots += i*v.Number + 1
+		tuples[i].IndexShot = i+1
+	}
+
+	fmt.Println(shots)
+	for _, t := range tuples {
+		fmt.Printf("%d ", t.Index+1)
+	}
+}
