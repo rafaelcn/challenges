ebf47768cdf5085baf202a8b1a111eba938d92e1
Rafael Campos Nunes <rcamposnunes@outlook.com>
2023-12-14 21:55:02 -0300

    Fix problem one counting an empty line
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- aoc/2023/go/1.go
+++ aoc/2023/go/1.go
@@ -13,17 +13,18 @@ func main() {
 	for {
 		line := ""
 
-		_, err := fmt.Scanf("%s", &line)
-		if err == io.EOF {
+		n, err := fmt.Scanf("%s", &line)
+		if err == io.EOF || n == 0 {
 			break
 		}
 
 		first := -1
 		last := -1
+
 		// naive solution
 		for _, character := range line {
 			if unicode.IsDigit(character) {
-				if first == -1 { // first hasn't been set
+				if first == -1 {
 					first, _ = strconv.Atoi(string(character))
 				} else {
 					last, _ = strconv.Atoi(string(character))
