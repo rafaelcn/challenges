af59ec13d0f4b76da7602a6a2112528fc73f0718
Rafael Campos Nunes <rcamposnunes@outlook.com>
2023-12-14 21:54:43 -0300

    Fix problem one part two
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- aoc/2023/go/1.2.go
+++ aoc/2023/go/1.2.go
@@ -11,35 +11,39 @@ func main() {
 	sum := 0
 
 	// o t t f f s s e n
-	numbers := map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
+	numbers := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
 	}
 
 	for {
 		line := ""
-		fixed := "" // the fixed line
 
-		_, err := fmt.Scanf("%s", &line)
-		if err == io.EOF {
+		n, err := fmt.Scanf("%s", &line)
+		if err == io.EOF || n == 0 {
 			break
 		}
 
+		j := 0
+		fl := [2]int{}
+
 		for i := 0; i < len(line); i++ {
-			if unicode.IsDigit(rune(line[i])) {
-				fixed += string(line[i])
-				continue
+			// check before because of the continue condition below
+			if j == 0 && fl[j] != 0 {
+				j++
 			}
 
-			// for each letter, try all numbers (which might not be that bad at)
-			matched := false
+			if unicode.IsDigit(rune(line[i])) {
+				fl[j], _ = strconv.Atoi(string(line[i]))
+				continue
+			}
 
 			for k, v := range numbers {
 				if i+len(k) > len(line) {
@@ -49,42 +53,18 @@ func main() {
 				//fmt.Printf("i: %d, l: %d, i+len(k): %d | ", i, len(line), i+len(k))
 				//fmt.Println(k, line[i:i+len(k)])
 				if line[i:i+len(k)] == k {
-					fixed += v
-					i += len(k) - 1
-					matched = true
+					i += len(k) - 2 // numbers that share a letter will be considered!
+					fl[j] = v
 					break
 				}
-
-			}
-
-			if !matched {
-				fixed += string(line[i])
-			}
-
-		}
-
-		// build the result
-		first := -1
-		last := -1
-
-		fmt.Println(fixed)
-
-		for _, character := range fixed {
-			if unicode.IsDigit(character) {
-				if first == -1 { // first hasn't been set
-					first, _ = strconv.Atoi(string(character))
-				} else {
-					last, _ = strconv.Atoi(string(character))
-				}
 			}
 		}
 
-		if last == -1 { // if the last number hasn't been set yet
-			last = first
+		if fl[1] == 0 {
+			fl[1] = fl[0]
 		}
 
-		sum += (first * 10) + last
-
+		sum += fl[0]*10 + fl[1]
 	}
 
 	fmt.Println(sum)
